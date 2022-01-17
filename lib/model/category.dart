b63a998ec1378cbcbbb22d8b076c8b928d4d0248
GAUTAM <gautam@gmail.com>
2022-01-17 16:35:38 +0530

    Model class completed

---
+++ lib/model/category.dart
@@ -0,0 +1,19 @@
+
+class Category
+{
+  int catId;
+  String catName;
+
+  Category({required this.catId,required this.catName});
+
+  factory  Category.fromJson(Map<String,dynamic> map)
+  {
+    return Category(
+      catId : map["catId"],
+      catName : map["catName"],
+
+    );
+  }
+
+
+}
